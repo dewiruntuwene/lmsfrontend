3a21dee02f9924a70ff9767f87a7db495dea8ffa
dewiruntuwene <runtuwenedewi@gmail.com>
2023-03-30 22:47:33 +0700

    Updates

---
+++ assets/logout-3fe6a222.js
@@ -0,0 +1,1 @@
+import{_ as o,r as t,o as n,c,b as r,F as s,a}from"./index-2a67faa6.js";const _={},l=a("div",null," logout ",-1);function u(d,i){const e=t("RouterView");return n(),c(s,null,[l,r(e)],64)}const f=o(_,[["render",u]]);export{f as default};
--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './app.vue'
-import router from './router'
-
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
